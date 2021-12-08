8661a5f27a89e517de92b43660cde54507d931fc
Chad Hogg <chadhogg@gmail.com>
2021-12-08 13:55:36 -0500

    Got 2021 day 08 working.

--- 2021/c++/2021Day08.cpp
+++ 2021/c++/2021Day08.cpp
@@ -11,6 +11,7 @@
 #include <climits>
 #include <cassert>
 #include <map>
+#include <set>
 
 #include "utilities.hpp"
 
@@ -140,38 +141,223 @@ SignalPattern const& findPattern (Display const& disp, unsigned int num) {
     throw std::runtime_error ("Cannot determine " + std::to_string (num) + " without further analysis.");
 }
 
-char determineA (SignalPattern const& one, SignalPattern const& seven) {
-    if (one.find (seven.at (0)) == std::string::npos) {
-        return seven.at (0);
-    }
-    else if(one.find (seven.at (1)) == std::string::npos) {
-        return seven.at (1);
-    }
-    else if(one.find (seven.at (2)) == std::string::npos) {
-        return seven.at (2);
-    }
-    else {
-        throw std::runtime_error ("Somehow, every position in 7 was also used in 1?");
-    }
+
+
+
+/*
+
+SignalPattern make9 (SignalPattern const& four, char a) {
+    SignalPattern nine = four;
+    nine += a;
+    std::sort (nine.begin (), nine.end ());
+    return nine;
 }
 
-int decode (Display const& display) {
-    std::map<char, char> crossing;
-    SignalPattern one = findPattern (display, 1);
-    SignalPattern four = findPattern (display, 4);
-    SignalPattern seven = findPattern (display, 7);
-    crossing['a'] = determineA (one, seven);
-    std::cout << "One is " << one << "\n";
-    std::cout << "Seven is " << seven << "\n";
-    std::cout << "Segment a comes from " << crossing['a'] << "\n";
-    // TODO
-    return 0;
+SignalPattern determine0 (SignalPattern const& one, Display const& display) {
+    bool found = false;
+    SignalPattern ret;
+    for (SignalPattern const& sig : display.patterns) {
+        if (sig.size () == 6 && sig.find (one.at (0)) != std::string::npos && sig.find (one.at (1)) != std::string::npos) {
+            if (found) {
+                std::cout << ret << "\n";
+                std::cout << sig << "\n";
+                std::cout << one << "\n";
+                throw std::runtime_error ("Find more than one size-6 pattern that contained all elements of 1.");
+            }
+            found = true;
+            ret = sig;
+        }
+    }
+    if (!found) {
+        throw std::runtime_error("Couldn't find 0.");
+    }
+    return ret;
+}
+
+SignalPattern determine6 (SignalPattern const& zero, Display const& display) {
+    bool found = false;
+    SignalPattern ret;
+    for (SignalPattern const& sig : display.patterns) {
+        if (sig.size () == 6 && sig != zero) {
+            if (found) {
+                throw std::runtime_error ("Found more than one size-6 pattern that wasn't 0.");
+            }
+            found = true;
+            ret = sig;
+        }
+    }
+    if (!found) {
+        throw std::runtime_error("Couldn't find 6.");
+    }
+    return ret;
+}
+
+SignalPattern determine5 (SignalPattern const& nine, SignalPattern const& one, Display const& display) {
+    bool found = false;
+    SignalPattern ret;
+    for (SignalPattern const& sig : display.patterns) {
+        if (sig.size () == 5 && sig != nine && sig.find (one.at (0)) != std::string::npos && sig.find (one.at (1)) != std::string::npos) {
+            if (found) {
+                throw std::runtime_error ("Found multiple size-5 signals that were'nt nine but contained all parts of 1.");
+            }
+            found = true;
+            ret = sig;
+        }
+    }
+    if (!found) {
+        throw std::runtime_error ("Couldn't find 5.");
+    }
+    return ret;
+}
+
+*/
+
+
+
+
+
+
+
+
+
+char missingLetter (SignalPattern const& hasIt, SignalPattern const& missingIt) {
+    bool found = false;
+    char ret = ' ';
+    for (char c : hasIt) {
+        if (missingIt.find (c) == std::string::npos) {
+            if (found) {
+                throw std::runtime_error (missingIt + " was missing more than one letter from " + hasIt);
+            }
+            found = true;
+            ret = c;
+        }
+    }
+    if (!found) {
+        throw std::runtime_error (missingIt + " was not missing any letters in " + hasIt);
+    }
+    return ret;
+}
+
+
+bool containsAll (SignalPattern const& super, SignalPattern const& sub) {
+    for (char c : sub) {
+        if (super.find (c) == std::string::npos) {
+            return false;
+        }
+    }
+    return true;
+}
+
+SignalPattern findOnlySupersetOfSize (Display const& display, SignalPattern const& sub, size_t size) {
+    bool found = false;
+    SignalPattern ret;
+    for (SignalPattern const& patt : display.patterns) {
+        if (patt.size () == size && containsAll (patt, sub)) {
+            if (found) {
+                throw std::runtime_error ("Found multiple size " + std::to_string (size) + " patterns that were supersets of " + sub + ": " + ret + " and " + patt);
+            }
+            found = true;
+            ret = patt;
+        }
+    }
+    if (!found) {
+        throw std::runtime_error ("Could not find a size " + std::to_string (size) + " pattern that is a superset of " + sub);
+    }
+    return ret;
+}
+
+SignalPattern findOnlySuperOfSizeExcluding (Display const& display, SignalPattern const& sub, size_t size, std::set<SignalPattern> const& exclusions) {
+    bool found = false;
+    SignalPattern ret;
+    for (SignalPattern const& patt : display.patterns) {
+        if (patt.size () == size && containsAll (patt, sub) && exclusions.count (patt) == 0) {
+            if (found) {
+                throw std::runtime_error ("Found multiple size " + std::to_string (size) + " patterns that were supersets of " + sub + "and not excluded: " + ret + " and " + patt);
+            }
+            found = true;
+            ret = patt;
+        }
+    }
+    if (!found) {
+        throw std::runtime_error ("Could not find a size " + std::to_string (size) + " pattern that is a superset of " + sub + " but is not excluded");
+    }
+    return ret;
+}
+
+SignalPattern findOnlySubOfSizeExcluding (Display const& display, SignalPattern const& super, size_t size, std::set<SignalPattern> const& exclusions) {
+    bool found = false;
+    SignalPattern ret;
+    for (SignalPattern const& patt : display.patterns) {
+        if (patt.size () == size && containsAll (super, patt) && exclusions.count (patt) == 0) {
+            if (found) {
+                throw std::runtime_error ("Found multiple size " + std::to_string (size) + " patterns that were subsets of " + super + "and not excluded: " + ret + " and " + patt);
+            }
+            found = true;
+            ret = patt;
+        }
+    }
+    if (!found) {
+        throw std::runtime_error ("Could not find a size " + std::to_string (size) + " pattern that is a subset of " + super + " but is not excluded");
+    }
+    return ret;
+}
+
+
+
+std::map<unsigned int, SignalPattern> decode (Display const& display) {
+    std::map<unsigned int, SignalPattern> intToPattern;
+
+    // i: The pattern of length 2 is #1, of length 3 is #7, of length 4 is #4, of length 7 is #8
+    intToPattern[1] = findPattern (display, 1);
+    intToPattern[4] = findPattern (display, 4);
+    intToPattern[7] = findPattern (display, 7);
+    intToPattern[8] = findPattern (display, 8);
+
+    // ii: The only length-6 pattern that contains all of #4 would be #9.
+    intToPattern[9] = findOnlySupersetOfSize (display, intToPattern[4], 6);
+
+    // Note: At this point we can figure out which signal is G, but I don't think that helps us.
+
+    // iii: The only length-6 pattern other than #9 that contains all of #1 would be #0.
+    intToPattern[0] = findOnlySuperOfSizeExcluding (display, intToPattern[1], 6, {intToPattern[9]});
+
+    // iv: The only length-6 pattern that isn't #0 or #9 is #6.
+    intToPattern[6] = findOnlySuperOfSizeExcluding (display, "", 6, {intToPattern[0], intToPattern[9]});
+
+    // v: The only length-5 pattern that is a superset of #1 is #3.
+    intToPattern[3] = findOnlySuperOfSizeExcluding (display, intToPattern[1], 5, {});
+
+    // vi: The only length-5 pattern that is a subset of #6 is #5.
+    intToPattern[5] = findOnlySubOfSizeExcluding (display, intToPattern[6], 5, {});
+
+    // vii: The only remaining length-5 pattern is #2.
+    intToPattern[2] = findOnlySuperOfSizeExcluding (display, "", 5, {intToPattern[3], intToPattern[5]}); 
+
+    return intToPattern;
+}
+
+unsigned int interpret (Display const& display, std::map<unsigned int, SignalPattern> const& mapping) {
+    unsigned int total {0U};
+    unsigned int factor {1000U};
+    for (unsigned int index {0U}; index < display.shown.size (); ++index) {
+        SignalPattern const& pattern = display.shown.at (index);
+        for (auto it = mapping.begin (); it != mapping.end (); ++it) {
+            if ((*it).second == pattern) {
+                total += factor * (*it).first;
+                factor /= 10U;
+            }
+        }
+    }
+    return total;
 }
 
 void part2 (Problem const& prob) {
     unsigned long total {0UL};
     for (Display const& disp : prob) {
-        total += decode (disp);
+        std::map<unsigned int, SignalPattern> solution = decode (disp);
+        unsigned int result = interpret (disp, solution);
+        //std::cout << result << "\n";
+        total += result;
     }
     std::cout << total << "\n";
 }
