0858c4bf5d58f9e3b4e00da616035433b691533a
Chad Hogg <chadhogg@gmail.com>
2021-12-19 13:47:41 -0500

    Got 2021 Day 19 to be able to find potentially overlapping beacons.

--- 2021/c++/2021Day19.cpp
+++ 2021/c++/2021Day19.cpp
@@ -20,14 +20,15 @@
 #include "Vector3.hpp"
 
 using Beacon = Vector3;
+using IndexPair = std::pair<unsigned int, unsigned int>;
 
 struct Scanner {
     unsigned int m_id;
     std::vector<Beacon> m_beacons;
-    std::unordered_map<float, std::pair<unsigned int, unsigned int>> m_distances;
+    std::unordered_map<float, IndexPair> m_distances;
 };
 
-using SomeScanners = std::vector<Scanner>;
+using SomeScanners = std::unordered_map<unsigned int, Scanner>;
 
 SomeScanners getInput () {
     SomeScanners scanners;
@@ -38,7 +39,7 @@ SomeScanners getInput () {
     while (std::getline (std::cin, line)) {
         if (line == "") {
             assert (workingOnScanner);
-            scanners.push_back (scanner);
+            scanners.insert ({scanner.m_id, scanner});
             workingOnScanner = false;
         }
         else if (sscanf (line.c_str (), "--- scanner %u ---", &id) == 1) {
@@ -56,26 +57,54 @@ SomeScanners getInput () {
         }
     }
     if (workingOnScanner) {
-        scanners.push_back (scanner);
+        scanners.insert ({scanner.m_id, scanner});
     }
 
-    for (Scanner & current : scanners) {
-        for (unsigned int index1 {0U}; index1 < current.m_beacons.size (); ++index1) {
-            for (unsigned int index2 {index1 + 1U}; index2 < current.m_beacons.size (); ++index2) {
-                Vector3 oneToOther = current.m_beacons.at (index1) - current.m_beacons.at (index2);
+    for (std::pair<const unsigned int, Scanner> & current : scanners) {
+        for (unsigned int index1 {0U}; index1 < current.second.m_beacons.size (); ++index1) {
+            for (unsigned int index2 {index1 + 1U}; index2 < current.second.m_beacons.size (); ++index2) {
+                Vector3 oneToOther = current.second.m_beacons.at (index1) - current.second.m_beacons.at (index2);
                 float distance = oneToOther.length ();
-                assert (current.m_distances.find (distance) == current.m_distances.end ());
-                current.m_distances.insert ({distance, {index1, index2}});
+                assert (current.second.m_distances.find (distance) == current.second.m_distances.end ());
+                current.second.m_distances.insert ({distance, {index1, index2}});
             }
         }
     }
     return scanners;
 }
 
+
+std::vector<std::pair<IndexPair, IndexPair>> findSameDistanceBeacons (Scanner const& done, Scanner const& other) {
+    std::vector<std::pair<IndexPair, IndexPair>> beaconPairPairs;
+    for (std::pair<float, IndexPair> const& donePair : done.m_distances) {
+        auto iter = other.m_distances.find (donePair.first);
+        if (iter != other.m_distances.end ()) {
+            beaconPairPairs.push_back ({donePair.second, (*iter).second});
+        }
+    }
+    return beaconPairPairs;
+}
+
+void searchForOverlappingScanners (SomeScanners & finished, SomeScanners & outstanding) {
+    for (std::pair<unsigned int, Scanner> done : finished) {
+        for (std::pair<unsigned int, Scanner> other : outstanding) {
+            std::vector<std::pair<IndexPair, IndexPair>> matches = findSameDistanceBeacons (done.second, other.second);
+            std::cout << "From " << done.first << " to " << other.first << " found " << matches.size () << " matches.\n";
+        }
+    }
+}
+
+
 /// \brief Runs the prorgram.
 /// \return Always 0.
 int main () {
     SomeScanners problem = getInput ();
     std::cout << problem.size () << "\n";
+    SomeScanners finished;
+    SomeScanners outstanding;
+    finished.insert ({0U, problem.at (0U)});
+    outstanding = problem;
+    outstanding.erase (0U);
+    searchForOverlappingScanners (finished, outstanding);
     return 0;
 }
