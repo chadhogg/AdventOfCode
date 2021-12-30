64a726f33aff0a094a07cb3b1d97e3aa042195f7
Chad Hogg <chadhogg@gmail.com>
2021-12-30 13:57:44 -0500

    Solved 2019 day 14 part 2.

--- 2019/c++/2019Day14.cpp
+++ 2019/c++/2019Day14.cpp
@@ -4,8 +4,6 @@
 
 #include <iostream>
 #include <string>
-#include <cassert>
-#include <cstdio>
 #include <unordered_map>
 
 #include "utilities.hpp"
@@ -136,9 +134,76 @@ unsigned long oreToMakeFuel (Cookbook const& book) {
     return oreAmount;
 }
 
+unsigned long howMuchFuelFromTrillionOre (Cookbook const& book) {
+    std::unordered_map<std::string, unsigned long> needs;
+    std::unordered_map<std::string, unsigned long> extras;
+    unsigned long oreAmount {0UL};
+    unsigned long fuelMade {0UL};
+    while (oreAmount <= 1'000'000'000'000) {
+        //std::cout << "Made " << fuelMade << " fuel, used " << oreAmount << " ore.\n";
+        unsigned long jump {1UL};
+        if (oreAmount < 900'000'000'000) {
+            jump = 1000UL;
+            //std::cout << "Leaping by 1000\n";
+        }
+        else if (oreAmount < 999'900'000'000) {
+            jump = 100UL;
+            //std::cout << "Jumping by 100\n";
+        }
+        needs.insert ({"FUEL", jump});
+        fuelMade += jump;
+        while (!needs.empty ()) {
+            //std::cout << "Needs: " << needs << "\n";
+            //std::cout << "Extras: " << extras << "\n";
+            std::string neededType = needs.begin ()->first;
+            unsigned long neededQuantity = needs.begin ()->second;
+            needs.erase (needs.begin ());
+            Recipe const& recipe = book.at (neededType);
+            unsigned long times = neededQuantity / recipe.output.quantity + (neededQuantity % recipe.output.quantity == 0 ? 0 : 1);
+            unsigned long surplus = times * recipe.output.quantity - neededQuantity;
+            if (surplus > 0) {
+                if (extras.count (neededType) == 0) {
+                    extras.insert ({neededType, surplus});
+                }
+                else {
+                    extras[neededType] += surplus;
+                }
+            }
+            for (Ingredient const& component : recipe.inputs) {
+                unsigned long needed = times * component.quantity;
+                if (component.type == "ORE") {
+                    oreAmount += needed;
+                }
+                else {
+                    if (extras.count (component.type) == 1) {
+                        if (extras.at (component.type) > needed) {
+                            extras[component.type] = extras[component.type] - needed;
+                            needed = 0;
+                        }
+                        else {
+                            needed -= extras[component.type];
+                            extras.erase (component.type);
+                        }
+                    }
+                    if (needed > 0) {
+                        if (needs.count (component.type) == 0) {
+                            needs.insert ({component.type, needed});
+                        }
+                        else {
+                            needs[component.type] += needed;
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return fuelMade - 1;
+}
+
 int main () {
     Cookbook book = getInput ();
     //std::cout << book << "\n";
     std::cout << oreToMakeFuel (book) << "\n";
+    std::cout << howMuchFuelFromTrillionOre (book) << "\n";
     return 0;
 }
