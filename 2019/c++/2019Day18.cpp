94a66359ec88f75c763d00f6c5db034afd6170a0
Chad Hogg <chadhogg@gmail.com>
2022-01-02 11:23:03 -0500

    Solved 2019 day 18 part 2, but it takes 86 minutes!

--- 2019/c++/2019Day18.cpp
+++ 2019/c++/2019Day18.cpp
@@ -19,17 +19,17 @@ constexpr char PLAYER = '@';
 constexpr char WALL = '#';
 constexpr char PASSAGE = '.';
 constexpr char OFF_MAP = '?';
+constexpr char PLAYER2 = '2';
+constexpr char PLAYER3 = '3';
+constexpr char PLAYER4 = '4';
 
-template<typename T>
-using Grid = std::vector<std::vector<T>>;
-using Vault = Grid<char>;
 
 struct GameState {
-    Vault picture;
+    std::vector<std::vector<char>> picture;
     std::map<char, Coordinate> keyLocations;
     std::map<char, Coordinate> doorLocations;
     std::set<char> foundKeys;
-    Coordinate playerLoc;
+    std::vector<Coordinate> playerLocs;
     unsigned int movedSoFar;
 
     char getUnderlyingSymbol (Coordinate const& where) const {
@@ -53,7 +53,9 @@ struct GameState {
     char getSymbol (Coordinate const& where) const {
         if (where.row < 0 || where.col < 0 || where.row >= (int)picture.size () || where.col >= (int)picture[where.row].size ()) { return OFF_MAP; }
         else {
-            if (playerLoc == where) { return PLAYER; }
+            for (Coordinate const& c : playerLocs) {
+                if (c == where) { return PLAYER; }
+            }
             for (std::pair<char, Coordinate> key : keyLocations) {
                 if (key.second == where && foundKeys.count (key.first) == 0) {
                     return key.first;
@@ -105,7 +107,7 @@ GameState getInput () {
                 state.picture.back ().push_back (PASSAGE);
             }
             else if (symbol == PLAYER) {
-                state.playerLoc = {row, col};
+                state.playerLocs.push_back ({row, col});
                 state.picture.back ().push_back (PASSAGE);
             }
             else {
@@ -121,41 +123,6 @@ GameState getInput () {
 std::vector<Coordinate> getNeighbors (Coordinate c) {
     return { {c.row - 1, c.col}, {c.row + 1, c.col}, {c.row, c.col - 1}, {c.row, c.col + 1} };
 }
-/*
-class ReachablilityCache {
-public:
-
-    bool isKnown (char source, char destination) const {
-        return internal.count (source) == 1 && internal.at (source).count (destination) == 1;
-    }
-
-    void store (char source, char destination, std::set<char> const& neededKeys, unsigned int distance) {
-        if (internal.count (source) == 0) { internal.insert ({source, {}}); }
-        assert (internal[source].count (destination) == 0);
-        internal[source][destination] = {neededKeys, distance};
-    }
-
-    std::set<char> const& getNeededKeys (char source, char destination) const {
-        assert (isKnown (source, destination));
-        return internal.at (source).at (destination).first;
-    }
-
-    unsigned int getDistance (char source, char destination) const {
-        assert (isKnown (source, destination));
-        return internal.at (source).at (destination).second;
-    }
-
-private:
-    std::map<char, std::map<char, std::pair<std::set<char>, unsigned int>>> internal;
-};
-
-std::vector<std::pair<Coordinate, unsigned int>> getReachableKeys (GameState const& state) {
-    static ReachablilityCache cache;
-    std::vector<std::pair<Coordinate, unsigned int>> result;
-    // todo
-    return result;
-}
-*/
 
 template<typename T>
 bool isSubset (std::set<T> const& whole, std::set<T> const& part) {
@@ -175,16 +142,16 @@ Coordinate findClosest (std::unordered_map<Coordinate, unsigned int> const& map)
     return chosen;
 }
 
-std::vector<std::pair<Coordinate, unsigned int>> getReachableKeys (GameState const& state) {
+std::vector<std::pair<Coordinate, unsigned int>> getReachableKeys (GameState const& state, unsigned int playerIndex) {
     static std::unordered_map<Coordinate, std::vector<std::pair<Coordinate, std::pair<std::set<char>, unsigned int>>>> cache;
-    if (cache.count (state.playerLoc) == 0) {
-        cache[state.playerLoc] = {};
+    if (cache.count (state.playerLocs[playerIndex]) == 0) {
+        cache[state.playerLocs[playerIndex]] = {};
         std::unordered_map<Coordinate, unsigned int> finished;
         std::unordered_map<Coordinate, unsigned int> unfinished;
         std::unordered_map<Coordinate, std::set<char>> keysNeeded;
-        unfinished.insert ({state.playerLoc, 0U});
-        keysNeeded.insert ({state.playerLoc, {}});
-        while (cache[state.playerLoc].size () < state.keyLocations.size ()) {
+        unfinished.insert ({state.playerLocs[playerIndex], 0U});
+        keysNeeded.insert ({state.playerLocs[playerIndex], {}});
+        while (!unfinished.empty ()) {
             Coordinate currentLoc = findClosest (unfinished);
             unsigned int currentDist = unfinished[currentLoc];
             unfinished.erase (currentLoc);
@@ -203,160 +170,98 @@ std::vector<std::pair<Coordinate, unsigned int>> getReachableKeys (GameState con
             }
             finished[currentLoc] = currentDist;
             if (std::islower (state.getUnderlyingSymbol (currentLoc))) {
-                cache[state.playerLoc].push_back ({currentLoc, {keysNeeded[currentLoc], currentDist}});
+                cache[state.playerLocs[playerIndex]].push_back ({currentLoc, {keysNeeded[currentLoc], currentDist}});
             }
         }
     }
-    /* actually slowed things down, despite finding that there was lots of repetition
-    static std::unordered_map<Coordinate, std::map<std::set<char>, std::vector<std::pair<Coordinate, unsigned int>>>> cache2;
-    if (cache2.count (state.playerLoc) == 0) { cache2[state.playerLoc] = {}; }
-    if (cache2[state.playerLoc].count (state.foundKeys) == 0) {
-    */
     std::vector<std::pair<Coordinate, unsigned int>> result;
-    for (std::pair<Coordinate, std::pair<std::set<char>, unsigned int>> const& pair : cache[state.playerLoc]) {
+    for (std::pair<Coordinate, std::pair<std::set<char>, unsigned int>> const& pair : cache[state.playerLocs[playerIndex]]) {
         if (state.foundKeys.count (state.getUnderlyingSymbol (pair.first)) == 0) {
             if (isSubset (state.foundKeys, pair.second.first)) {
                 result.push_back ({pair.first, pair.second.second});
             }
         }
     }
-    /*
-    cache2[state.playerLoc][state.foundKeys] = result;
-    }
-    return cache2[state.playerLoc][state.foundKeys];
-    */
-    //std::cout << "Reachable from " << state.playerLoc << " is " << result << "\n";
     return result;
 }
 
-/*
-std::unordered_map<Coordinate, unsigned int> getPathLengths (GameState const& state) {
-    std::unordered_map<Coordinate, unsigned int> keyDistances;
-    std::unordered_map<Coordinate, unsigned int> finished;
-    std::unordered_map<Coordinate, unsigned int> unfinished;
-    unfinished.insert ({state.playerLoc, 0U});
-    while (!unfinished.empty ()) {
-        Coordinate currentLoc = findClosest (unfinished);
-        unsigned int currentDist = unfinished[currentLoc];
-        unfinished.erase (currentLoc);
-        std::vector<Coordinate> neighbors = getNeighbors (currentLoc);
-        for (Coordinate neighbor : neighbors) {
-            if (finished.count (neighbor) == 0) {
-                char symbol = state.getSymbol (neighbor);
-                if (symbol == PASSAGE || std::islower (symbol)) {
-                    if (unfinished.count (neighbor) == 0 || unfinished[neighbor] > currentDist + 1) {
-                        unfinished[neighbor] = currentDist + 1;
-                    }
-                }
-            }
-        }
-        finished[currentLoc] = currentDist;
-        if (std::islower (state.getSymbol (currentLoc))) {
-            keyDistances[currentLoc] = currentDist;
-        }
+template<typename T>
+bool same (std::vector<T> const& x, std::vector<T> const& y) {
+    if (x.size () != y.size ()) { return false; }
+    for (unsigned int index {0U}; index < x.size (); ++index) {
+        if (!(x[index] == y[index])) { return false; }
     }
-    return keyDistances;
-}
-
-unsigned int findAllKeys (GameState & state, unsigned int cutoff) {
-    if (state.foundKeys.size () == state.keyLocations.size ()) { 
-        std::cout << state << "\n";
-        return state.movedSoFar;
-    }
-    unsigned int lowestChild {cutoff};
-    std::unordered_map<Coordinate, unsigned int> possibleMoves = getPathLengths (state);
-    std::vector<std::pair<Coordinate, unsigned int>> sortedMoves;
-    for (std::pair<const Coordinate, unsigned int> const& move : possibleMoves) {
-        bool inserted {false};
-        for (unsigned int index {0U}; index < sortedMoves.size () && !inserted; ++index) {
-            if (move.second < sortedMoves[index].second) {
-                sortedMoves.insert (sortedMoves.begin () + index, move);
-                inserted = true;
-            }
-        }
-        if (!inserted) { sortedMoves.push_back (move); }
-    }
-    for (std::pair<Coordinate, unsigned int> const& move : sortedMoves) {
-        if (state.movedSoFar + move.second < cutoff) {
-            Coordinate oldPlayerLoc = state.playerLoc;
-            char key = state.getSymbol (move.first);
-            assert (std::islower (key) && state.foundKeys.count (key) == 0);
-            state.foundKeys.insert (key);
-            state.playerLoc = move.first;
-            state.movedSoFar += move.second;
-            lowestChild = std::min (lowestChild, findAllKeys (state, lowestChild));
-            state.movedSoFar -= move.second;
-            state.playerLoc = oldPlayerLoc;
-            state.foundKeys.erase (key);
-        }
-    }
-    return lowestChild;
-}
-*/
-
-unsigned int findAllKeys (GameState & state, unsigned int cutoff) {
-    if (state.foundKeys.size () == state.keyLocations.size ()) { 
-        std::cout << state << "\n";
-        return state.movedSoFar;
-    }
-    unsigned int lowestChild {cutoff};
-    std::vector<std::pair<Coordinate, unsigned int>> possibleMoves = getReachableKeys (state);
-    for (std::pair<Coordinate, unsigned int> const& move : possibleMoves) {
-        if (state.movedSoFar + move.second < cutoff) {
-            Coordinate oldPlayerLoc = state.playerLoc;
-            char key = state.getSymbol (move.first);
-            assert (std::islower (key) && state.foundKeys.count (key) == 0);
-            state.foundKeys.insert (key);
-            state.playerLoc = move.first;
-            state.movedSoFar += move.second;
-            lowestChild = std::min (lowestChild, findAllKeys (state, lowestChild));
-            state.movedSoFar -= move.second;
-            state.playerLoc = oldPlayerLoc;
-            state.foundKeys.erase (key);
-        }
-    }
-    return lowestChild;
+    return true;
 }
 
 unsigned int findAllKeysBF (GameState const& initial) {
     std::deque<GameState *> frontier;
     unsigned int bestFinished {UINT_MAX};
     frontier.push_back (new GameState (initial));
+    unsigned int count {0U};
+    unsigned int skipped {0U};
+    unsigned int keys = 0;
     while (!frontier.empty ()) {
+        ++count;
         GameState * state = frontier.front ();
         frontier.pop_front ();
+        if (state->foundKeys.size () > keys) {
+            std::cout << "Finished exploring paths with " << keys << " keys found, frontier has " << frontier.size () << " states, skipped " << skipped << " duplicates.\n";
+            ++keys;
+            skipped = 0U;
+        }
         if (state->movedSoFar >= bestFinished) { delete state; }
         else if (state->foundKeys.size () == state->keyLocations.size ()) {
             if (state->movedSoFar < bestFinished) { bestFinished = state->movedSoFar; }
             delete state;
         }
         else {
-            for (std::pair<Coordinate, unsigned int> const& move : getReachableKeys (*state)) {
-                GameState * successor = new GameState (*state);
-                successor->playerLoc = move.first;
-                successor->movedSoFar += move.second;
-                successor->foundKeys.insert (successor->getUnderlyingSymbol (move.first));
-                bool found = false;
-                for (GameState* existing : frontier) {
-                    if (existing->playerLoc == successor->playerLoc && existing->foundKeys == successor->foundKeys) {
-                        found = true;
-                        existing->movedSoFar = std::min (existing->movedSoFar, successor->movedSoFar);
-                        break;
+            for (unsigned int playerIndex {0U}; playerIndex < state->playerLocs.size (); ++playerIndex) {
+                for (std::pair<Coordinate, unsigned int> const& move : getReachableKeys (*state, playerIndex)) {
+                    GameState * successor = new GameState (*state);
+                    successor->playerLocs[playerIndex] = move.first;
+                    successor->movedSoFar += move.second;
+                    successor->foundKeys.insert (successor->getUnderlyingSymbol (move.first));
+                    bool found = false;
+                    for (GameState* existing : frontier) {
+                        if (/*existing->playerLocs[0] == successor->playerLocs[0]*/ same (existing->playerLocs, successor->playerLocs) /*existing->playerLocs == successor->playerLocs*/ && existing->foundKeys == successor->foundKeys) {
+                            found = true;
+                            existing->movedSoFar = std::min (existing->movedSoFar, successor->movedSoFar);
+                            ++skipped;
+                            break;
+                        }
                     }
+                    if (found) { delete successor; }
+                    else { frontier.push_back (successor); }
                 }
-                if (found) { delete successor; }
-                else { frontier.push_back (successor); }
             }
+            delete state;
         }
     }
+    std::cout << "Evaluated " << count << " states.\n";
     return bestFinished;
 }
 
+GameState divideMap (GameState const& original) {
+    GameState result (original);
+    Coordinate center = result.playerLocs[0];
+    result.picture[center.row][center.col] = WALL;
+    result.picture[center.row - 1][center.col] = WALL;
+    result.picture[center.row + 1][center.col] = WALL;
+    result.picture[center.row][center.col - 1] = WALL;
+    result.picture[center.row][center.col + 1] = WALL;
+    result.playerLocs[0] = {center.row - 1, center.col - 1};
+    result.playerLocs.push_back ({center.row - 1, center.col + 1});
+    result.playerLocs.push_back ({center.row + 1, center.col - 1});
+    result.playerLocs.push_back ({center.row + 1, center.col + 1});
+    return result;
+}
 
 int main () {
     GameState initial = getInput ();
-    std::cout << initial << "\n";
-    //std::cout << findAllKeys (initial, UINT_MAX) << "\n";
-    std::cout << findAllKeysBF (initial) << "\n";
+    //std::cout << initial << "\n";
+    //std::cout << findAllKeysBF (initial) << "\n";
+    GameState revised = divideMap (initial);
+    std::cout << findAllKeysBF (revised) << "\n";
     return 0;
 }
