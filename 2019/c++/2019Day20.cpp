630ddcf4d2647527929b279102eaaddc00298cd4
Chad Hogg <chadhogg@gmail.com>
2022-01-02 23:44:54 -0500

    Solved 2019 day 20 part 2.

--- 2019/c++/2019Day20.cpp
+++ 2019/c++/2019Day20.cpp
@@ -119,9 +119,77 @@ unsigned int getShortestPathLength (Board const& board) {
     return finished[board.goal];
 }
 
+struct RecCoord {
+    Coordinate coord;
+    int depth;
+};
+
+bool operator== (RecCoord const& x, RecCoord const& y) {
+    return x.depth == y.depth && x.coord == y.coord;
+}
+
+template<>
+struct std::hash<RecCoord> {
+    std::size_t operator()(RecCoord const& rc) const {
+        static std::hash<Coordinate> coordHasher;
+        return coordHasher (rc.coord) * 97 + rc.depth;
+    }
+};
+
+std::vector<RecCoord> getNeighbors (RecCoord c) {
+    return { {{c.coord.row - 1, c.coord.col}, c.depth}, {{c.coord.row + 1, c.coord.col}, c.depth}, {{c.coord.row, c.coord.col - 1}, c.depth}, {{c.coord.row, c.coord.col + 1}, c.depth} };
+}
+
+RecCoord findClosest (std::unordered_map<RecCoord, unsigned int> const& map) {
+    RecCoord chosen = map.begin ()->first;
+    for (std::pair<RecCoord const, unsigned int> const& pair : map) {
+        if (pair.second < map.at (chosen)) {
+            chosen = pair.first;
+        }
+    }
+    return chosen;
+}
+
+inline bool isInside (Board const& board, Coordinate const& where) {
+    return where.row > 2 && where.row < (int)board.picture.size () - 3 && where.col > 2 && where.col < (int)board.picture[0].size () - 3;
+}
+
+unsigned int getRecursiveShortestPathLength (Board const& board) {
+    std::unordered_map<RecCoord, unsigned int> finished;
+    std::unordered_map<RecCoord, unsigned int> unfinished;
+    unfinished.insert ({{board.start, 0}, 0U});
+    RecCoord goal {board.goal, 0};
+    while (finished.count (goal) == 0) {
+        assert (!unfinished.empty ());
+        RecCoord currentLoc = findClosest (unfinished);
+        unsigned int currentDist = unfinished[currentLoc];
+        unfinished.erase (currentLoc);
+        std::vector<RecCoord> neighbors = getNeighbors (currentLoc);
+        if (board.portals.count (currentLoc.coord) == 1) {
+            if (isInside (board, currentLoc.coord)) {
+                neighbors.push_back ({board.portals.at (currentLoc.coord), currentLoc.depth + 1});
+            }
+            else if (currentLoc.depth != 0) {
+                neighbors.push_back ({board.portals.at (currentLoc.coord), currentLoc.depth - 1});                
+            }
+        }
+        for (RecCoord neighbor : neighbors) {
+            if (finished.count (neighbor) == 0) {
+                if (board.picture[neighbor.coord.row][neighbor.coord.col] == PASSAGE) {
+                    if (unfinished.count (neighbor) == 0 || unfinished[neighbor] > currentDist + 1) {
+                        unfinished[neighbor] = currentDist + 1;
+                    }
+                }
+            }
+        }
+        finished[currentLoc] = currentDist;
+    }
+    return finished[goal];
+}
 
 int main () {
     Board board = getInput ();
     std::cout << "Shortest path start to goal: " << getShortestPathLength (board) << "\n";
+    std::cout << "Recursive version: " << getRecursiveShortestPathLength (board) << "\n";
     return 0;
 }
