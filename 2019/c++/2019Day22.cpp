ac1bb1d81814d56753feb87e76508c302fd58585
Chad Hogg <chadhogg@gmail.com>
2022-01-08 14:15:32 -0500

    Made an attempt at 2019 day 22 part 2, was not successful.

--- 2019/c++/2019Day22.cpp
+++ 2019/c++/2019Day22.cpp
@@ -10,6 +10,7 @@
 #include <numeric>
 #include <deque>
 #include <stack>
+#include <unordered_map>
 
 #include "utilities.hpp"
 
@@ -118,10 +119,83 @@ std::size_t findCard (Deck const& deck, Card card) {
     throw std::runtime_error ("Didn't find card.");
 }
 
+constexpr unsigned long long NUM_CARDS = 119315717514047ULL;
+constexpr unsigned long long TIMES_TO_DO = 101741582076661ULL;
+
+inline unsigned long long getPositionBeforeDealIntoNewStack (unsigned long long after) {
+    return NUM_CARDS - after - 1;
+}
+
+inline unsigned long long getPositionBeforeCutNCards (unsigned long long after, int n) {
+    if (n > 0) {
+        if (after >= NUM_CARDS - n) { return after - (NUM_CARDS - n); }
+        else { return after + n; }
+    }
+    else {
+        n = std::abs (n);
+        if (after < (unsigned int)n) { return after + (NUM_CARDS - n); }
+        else { return after - n; }
+    }
+}
+
+/*
+EXAMPLE
+size of 10, increment of 3
+old new
+0   0
+1   3
+2   6
+3   9
+4   2
+5   5
+6   8
+7   1
+8   4
+9   7
+if (new % inc == 0) then old = new / inc
+if (new + size % inc == 0), then old = new + size / inc
+if (new + 2size % inc == 0), then old = new + 2size / inc
+*/
+
+inline unsigned long long getPositionBeforeDealWithIncrementN (unsigned long long after, int n) {
+    for (unsigned int factor {1U}; true; ++factor) {
+        if ((after + factor * NUM_CARDS) % n == 0) { return (after + factor * NUM_CARDS) / n; }
+    }
+}
+
+unsigned long long findPositionBeforeAllSteps (unsigned long long finalPos, std::vector<Action> const& commands, unsigned long long repetitions) {
+    std::unordered_map<unsigned long long, unsigned long long> cache;
+    bool found = false;
+    unsigned long long pos = finalPos;
+    for (unsigned long long r {0U}; r < repetitions; ++r) {
+        unsigned long long afterThisCycle = pos;
+        if (r % 10000000ULL == 0ULL) {
+            std::cout << "Finished " << r / (double)repetitions * 100 << "% of repetitions.\n";
+        }
+        if (cache.count (afterThisCycle) == 1) {
+            pos = cache[afterThisCycle];
+            if (!found) { std::cout << "Hit a cycle!\n"; }
+            found = true;
+            break;
+        }
+        for (long index = commands.size () - 1; index >= 0; --index) {
+            switch (commands[index].type) {
+                case STACK: pos = getPositionBeforeDealIntoNewStack (pos); break;
+                case CUT: pos = getPositionBeforeCutNCards (pos, commands[index].param); break;
+                case DEAL: pos = getPositionBeforeDealWithIncrementN (pos, commands[index].param); break;
+                default: throw std::runtime_error ("Unknown command.");
+            }
+        }
+        cache[afterThisCycle] = pos;
+    }
+    return pos;
+}
+
 int main () {
     std::vector<Action> commands = getInput ();
     Deck deck = getFactoryOrder (10007);
     shuffle (commands, deck);
     std::cout << findCard (deck, 2019) << "\n";
+    std::cout << findPositionBeforeAllSteps (2020, commands, TIMES_TO_DO) << "\n";
     return 0;
 }
