b72732d8f9b0204db87fe163d97ff349532a8d7e
Chad Hogg <chadhogg@gmail.com>
2022-12-16 23:45:05 -0500

    Solved part 1 of 2022 day 16.

--- 2022/c++/2022Day16.cpp
+++ 2022/c++/2022Day16.cpp
@@ -2,6 +2,12 @@
 /// \author Chad Hogg
 /// \brief My solution to Advent Of Code for 2022-12-16.
 
+// Observation: Movements that don't end in opening a value accomplish nothing and are not worth doing.
+// Observation: I can pre-compute the shortest paths between any two valves.
+// These two together reduce the problem to finding the optimal order of values to open ignoring movement.
+// Of course, there are still 15! possible orders to consider.
+// A greedy heuristic always moves to the biggest closed valve (maybe opening others you pass by?)
+// Can we try that first and prune branches that are definitively worse than it?
 #include <iostream>
 #include <vector>
 #include <string>
@@ -10,6 +16,9 @@
 #include <algorithm>
 #include <map>
 #include <set>
+#include <stack>
+
+const unsigned MAX_MINUTES = 30;
 
 struct Valve
 {
@@ -22,16 +31,20 @@ struct Valve
 struct Problem
 {
   std::map<std::string, struct Valve> m_valves;
-  unsigned m_minutesLeft;
+};
+
+struct State
+{
+  std::set<std::string> m_openValves;
+  int m_minutesLeft;
   unsigned long m_pressureReleased;
+  std::string m_location;
 };
 
 Problem
 getInput ()
 {
   Problem prob;
-  prob.m_minutesLeft = 30;
-  prob.m_pressureReleased = 0;
   std::string line;
   std::string part1 = "Valve ";
   std::string part2 = " has flow rate=";
@@ -76,8 +89,85 @@ getInput ()
   return prob;
 }
 
+using PathDistances = std::map<std::string, std::map<std::string, unsigned>>;
+
+PathDistances floydWarshall (const Problem& prob)
+{
+  PathDistances result;
+  for (const auto& v1 : prob.m_valves)
+  {
+    result[v1.first] = {};
+    for (const auto& v2 : prob.m_valves)
+    {
+      if (v1.first == v2.first) {result[v1.first][v2.first] = 0; }
+      else if (v1.second.m_tunnels.count (v2.first) == 1) { result[v1.first][v2.first] = 1; }
+      else { result[v1.first][v2.first] = MAX_MINUTES + 1; }
+    }
+  }
+  for (const auto& v1 : prob.m_valves)
+  {
+    for (const auto& v2 : prob.m_valves)
+    {
+      for (const auto& v3 : prob.m_valves)
+      {
+        result[v2.first][v3.first] = std::min (result[v2.first][v3.first],
+          result[v2.first][v1.first] + result[v1.first][v3.first]);
+      }
+    }
+  }
+  return result;
+}
+
+void
+advanceState (const Problem& prob, State& state, int minutes)
+{
+  int pressurePerMinute = 0;
+  for (const std::string& valve : state.m_openValves)
+  {
+    pressurePerMinute += prob.m_valves.at (valve).m_flowRate;
+  }
+  state.m_pressureReleased += minutes * pressurePerMinute;
+  state.m_minutesLeft -= minutes;
+}
+
+State
+depthFirstSearch (const Problem& prob, const PathDistances& distances)
+{
+  State initial;
+  initial.m_location = "AA";
+  initial.m_minutesLeft = MAX_MINUTES;
+  initial.m_pressureReleased = 0;
+  State best;
+  std::stack<State> frontier;
+  frontier.push (initial);
+  while (!frontier.empty ())
+  {
+    State current = frontier.top ();
+    frontier.pop ();
+    for (const auto& valve : prob.m_valves)
+    {
+      if (valve.second.m_flowRate != 0 && current.m_openValves.count (valve.first) == 0)
+      {
+        State copy = current;
+        advanceState (prob, copy, distances.at (copy.m_location).at (valve.first) + 1);
+        copy.m_openValves.insert (valve.first);
+        copy.m_location = valve.first;
+        if (copy.m_minutesLeft >= 0)
+        {
+          frontier.push (copy);
+        }
+      }
+    }
+    advanceState (prob, current, current.m_minutesLeft);
+    if (current.m_pressureReleased > best.m_pressureReleased) { best = current; }
+  }
+  return best;
+}
+
+
 int main () {
   Problem prob = getInput ();
-
+  PathDistances distances = floydWarshall (prob);
+  std::cout << depthFirstSearch (prob, distances).m_pressureReleased << "\n";
   return 0;
 }
