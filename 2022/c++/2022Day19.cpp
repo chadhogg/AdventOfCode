cbaf74355dd9d41a73122e714161b1d3cd8246aa
Chad Hogg <chadhogg@gmail.com>
2022-12-21 11:02:41 -0500

    Some improvement of 2022 day 19, but still not fast enough.

--- 2022/c++/2022Day19.cpp
+++ 2022/c++/2022Day19.cpp
@@ -12,6 +12,8 @@
 
 using Number = unsigned long;
 
+const Number MAX_MINUTES = 24;
+
 enum Resource {ORE, CLAY, OBSIDIAN, GEODE};
 
 struct Recipe
@@ -53,7 +55,7 @@ getInput ()
     Number clayRobotOreCost = 0;
     Number obsidianRobotOreCost = 0, obsidianRobotClayCost = 0;
     Number geodeRobotOreCost = 0, geodeRobotObsidianCost = 0;
-    if (scanf ("Blueprint %ld:", &blueprintNum) != 1) { break; }
+    if (scanf (" Blueprint %ld:", &blueprintNum) != 1) { break; }
     if (scanf (" Each ore robot costs %ld ore.", &oreRobotOreCost) != 1) { assert (false); }
     if (scanf (" Each clay robot costs %ld ore.", &clayRobotOreCost) != 1) { assert (false); }
     if (scanf (" Each obsidian robot costs %ld ore and %ld clay.", &obsidianRobotOreCost, &obsidianRobotClayCost) != 2) { assert (false); }
@@ -61,10 +63,10 @@ getInput ()
     assert (blueprintNum == blueprints.size () + 1);
     Blueprint current;
     current.m_number = blueprintNum;
-    current.m_recipes[ORE] = {ORE, {{ORE, oreRobotOreCost}}};
-    current.m_recipes[CLAY] = {CLAY, {{ORE, clayRobotOreCost}}};
-    current.m_recipes[OBSIDIAN] = {OBSIDIAN, {{ORE, obsidianRobotOreCost}, {CLAY, obsidianRobotClayCost}}};
-    current.m_recipes[GEODE] = {GEODE, {{ORE, geodeRobotOreCost}, {OBSIDIAN, geodeRobotObsidianCost}}};
+    current.m_recipes[ORE] = {ORE, {{ORE, oreRobotOreCost}, {CLAY, 0}, {OBSIDIAN, 0}, {GEODE, 0}}};
+    current.m_recipes[CLAY] = {CLAY, {{ORE, clayRobotOreCost}, {CLAY, 0}, {OBSIDIAN, 0}, {GEODE, 0}}};
+    current.m_recipes[OBSIDIAN] = {OBSIDIAN, {{ORE, obsidianRobotOreCost}, {CLAY, obsidianRobotClayCost}, {OBSIDIAN, 0}, {GEODE, 0}}};
+    current.m_recipes[GEODE] = {GEODE, {{ORE, geodeRobotOreCost}, {CLAY, 0}, {OBSIDIAN, geodeRobotObsidianCost}, {GEODE, 0}}};
     blueprints.push_back (current);
   }
   return blueprints;
@@ -91,14 +93,43 @@ getProductivity (const State& current)
   return products;
 }
 
-State
-findBestEndingState (const Blueprint& blueprint, const State& current, Number maxMinutes)
+Number
+upperBound (const State& current)
 {
+  Number alreadyHas = current.m_stockpiles.at (GEODE);
+  Number increment = current.m_robots.at (GEODE);
+  Number extra = 0;
+  // Yeah, there's an n(n+1)/2 formula for this, but I'm hoping a bunch of additions isn't much worse than that.
+  for (Number minute = current.m_minutesPassed; minute < MAX_MINUTES; ++minute)
+  {
+    extra += increment;
+    ++increment;
+  }
+  return alreadyHas + extra;
+}
+
+void
+findBestEndingState (const Blueprint& blueprint, const State& current, State& bestSoFar)
+{
+  static Number pruned = 0;
+  if (current.m_minutesPassed == MAX_MINUTES)
+  {
+    if (current.m_stockpiles.at (GEODE) > bestSoFar.m_stockpiles.at (GEODE) || bestSoFar.m_minutesPassed < MAX_MINUTES)
+    {
+      bestSoFar = current;
+      //std::cout << bestSoFar << "\n";
+      //std::cout << "Pruned " << pruned << " nodes so far.\n";
+    }
+    return;
+  }
+  if (bestSoFar.m_minutesPassed == MAX_MINUTES && bestSoFar.m_stockpiles.at (GEODE) > upperBound (current))
+  {
+    ++pruned;
+    return;
+  }
   //std::cout << current << "\n";
-  if (current.m_minutesPassed == maxMinutes) { return current; }
   std::map<Resource, Number> production = getProductivity (current);
-  State best = current;
-  for (Resource robot : {GEODE, OBSIDIAN, CLAY, ORE})
+  for (Resource robot : std::vector<Resource> {GEODE, OBSIDIAN, CLAY, ORE})
   {
     if (canMake (blueprint, current, robot))
     {
@@ -106,14 +137,11 @@ findBestEndingState (const Blueprint& blueprint, const State& current, Number ma
       for (Resource thing : {ORE, CLAY, OBSIDIAN, GEODE})
       {
         next.m_stockpiles[thing] += production[thing];
+        next.m_stockpiles[thing] -= blueprint.m_recipes.at (robot).m_costs.at (thing);
       }
       ++next.m_robots[robot];
       ++next.m_minutesPassed;
-      State result = findBestEndingState (blueprint, next, maxMinutes);
-      if (result.m_minutesPassed > best.m_minutesPassed || result.m_stockpiles[GEODE] > best.m_stockpiles[GEODE])
-      {
-        best = result;
-      }
+      findBestEndingState (blueprint, next, bestSoFar);
     }
   }
   // Making no robot (only reasonable if we cannot afford one, I think).
@@ -123,12 +151,7 @@ findBestEndingState (const Blueprint& blueprint, const State& current, Number ma
     next.m_stockpiles[thing] += production[thing];
   }
   ++next.m_minutesPassed;
-  State result = findBestEndingState (blueprint, next, maxMinutes);
-  if (result.m_minutesPassed > best.m_minutesPassed || result.m_stockpiles[GEODE] > best.m_stockpiles[GEODE])
-  {
-    best = result;
-  }
-  return best;
+  findBestEndingState (blueprint, next, bestSoFar);
 }
 
 std::map<Number, Number>
@@ -138,7 +161,8 @@ computeMaxGeodesPerBlueprint (const std::vector<Blueprint>& blueprints)
   for (const Blueprint& bp : blueprints)
   {
     State current = {0, {{ORE, 0}, {CLAY, 0}, {OBSIDIAN, 0}, {GEODE, 0}}, {{ORE, 1}, {CLAY, 0}, {OBSIDIAN, 0}, {GEODE, 0}}};
-    State best = findBestEndingState (bp, current, 24);
+    State best = current;
+    findBestEndingState (bp, current, best);
     results[bp.m_number] = best.m_stockpiles[GEODE];
     std::cout << "Finished blueprint " << bp.m_number << " with result " << best.m_stockpiles[GEODE] << "\n";
   }
@@ -159,6 +183,7 @@ computeSumOfQualityLevels (const std::map<Number, Number>& maxGeodes)
 int main ()
 {
   std::vector<Blueprint> blueprints = getInput ();
+  std::cout << "Number of blueprints is: " << blueprints.size () << "\n";
   std::cout << computeSumOfQualityLevels (computeMaxGeodesPerBlueprint (blueprints)) << "\n";
   return 0;
 }
